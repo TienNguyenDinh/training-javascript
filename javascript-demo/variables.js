d52dbbc4faa2f8308b5afbf74c2066d2125d51b8
Tien Nguyen <mrfuree@gmail.com>
2024-01-04 16:30:47 +0700

    feat(js): add demo for variables

---
+++ javascript-demo/variables.js
@@ -0,0 +1,18 @@
+var a;
+a = 5;
+
+let b;
+b = 4;
+
+const c = 10;
+
+var d = 5;
+
+let e = 4;
+
+const x = "x", y = "y", z = "z";
+
+let ab, bc, ac;
+ab = bc = ac = "abc";
+
+const aa = bb = cc == "aabbcc";
