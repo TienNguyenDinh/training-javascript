72c9c2e893f662dee2ac852d494ca6d9051832b2
Tien Nguyen <mrfuree@gmail.com>
2024-01-05 09:29:56 +0700

    fix(js): replace all double quotes with single quotes

--- javascript-demo/variables.js
+++ javascript-demo/variables.js
@@ -10,18 +10,18 @@ var d = 5;
 
 let e = 4;
 
-const x = "x", y = "y", z = "z";
+const x = 'x', y = 'y', z = 'z';
 
 let ab, bc, ac;
-ab = bc = ac = "abc";
+ab = bc = ac = 'abc';
 
-const aa = "aabbcc";
+const aa = 'aabbcc';
 
-console.log("a: ", a);
-console.log("b: ", b);
-console.log("c: ", c);
-console.log("d: ", d);
-console.log("e: ", e);
-console.log("x, y, z: ", x, y, z);
-console.log("ab, bc, ac: ", ab, bc, ac);
-console.log("aa: ", aa);
+console.log('a: ', a);
+console.log('b: ', b);
+console.log('c: ', c);
+console.log('d: ', d);
+console.log('e: ', e);
+console.log('x, y, z: ', x, y, z);
+console.log('ab, bc, ac: ', ab, bc, ac);
+console.log('aa: ', aa);
