35c89ee62aedcc5d771c54a2c4edd2746ab524bc
Tien Nguyen <mrfuree@gmail.com>
2024-01-25 17:15:54 +0700

    feat(js): add content for events

--- javascript-demo/events.js
+++ javascript-demo/events.js
@@ -15,5 +15,49 @@ function saySth() {
   alert('tien day!!!!!!');
 }
 
-button3.addEventListener('click', () => alert('Alert again!'));
-button3.addEventListener('click', () => alert('Alert again!'));
+button3.addEventListener('click', saySth);
+button3.removeEventListener('click', saySth);
+
+const newButton = document.getElementById('newButton');
+
+newButton.onclick = function (event) {
+  console.log('Event type:' + event.type);
+  console.log('Current target:' + event.currentTarget);
+  console.log('Coordinates:' + event.clientX + ':' + event.clientY);
+};
+
+// Combine handlers into a class
+class MyClass {
+  handleEvent(event) {
+    switch (event.type) {
+      case 'mousedown':
+        newButton.textContent = 'Mouse button pressed';
+        break;
+      case 'mouseup':
+        newButton.textContent += '...and released.';
+        break;
+    }
+  }
+}
+
+let myClassInstance = new MyClass();
+
+newButton.addEventListener('mousedown', myClassInstance);
+newButton.addEventListener('mouseup', myClassInstance);
+
+// Bubbling
+const parent = document.getElementById('parent');
+const child = document.getElementById('child');
+
+parent.onclick = function () {
+  console.log('Clicked on parent');
+};
+
+child.onclick = function () {
+  console.log('Clicked on child');
+};
+
+document.addEventListener('click', (event) => {
+  console.log('x: ' + event.clientX);
+  console.log('y: ' + event.clientY);
+});
