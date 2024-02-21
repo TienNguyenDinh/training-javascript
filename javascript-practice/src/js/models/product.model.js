756fad61cd0c7f70e7c0893882d51c5cd0bf560c
Tien Nguyen <mrfuree@gmail.com>
2024-02-21 17:25:55 +0700

    feat(js): add product.model.js

---
+++ javascript-practice/src/js/models/product.model.js
@@ -0,0 +1,28 @@
+import ProductService from '../services/product.services';
+
+class Product {
+  constructor(id, name, currency, price, brand, modelName,
+    color, formFactor, connectivityTechnology, amount, imgPath) {
+    this.id = id;
+    this.name = name;
+    this.currency = currency;
+    this.price = price;
+    this.brand = brand;
+    this.modelName = modelName;
+    this.color = color;
+    this.formFactor = formFactor;
+    this.connectivityTechnology = connectivityTechnology;
+    this.amount = amount;
+    this.imgPath = imgPath;
+  }
+}
+
+class ProductModel {
+  constructor() {
+    this.productService = new ProductService();
+  }
+
+  async getProducts() {
+    return await productService.fetchProducts();
+  }
+}
