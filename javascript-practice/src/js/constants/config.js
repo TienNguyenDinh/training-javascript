9baf46f40211828ec8f56a09612d0237907e2fca
Tien Nguyen <mrfuree@gmail.com>
2024-02-22 16:56:28 +0700

    feat(js): add config.js constant values

---
+++ javascript-practice/src/js/constants/config.js
@@ -0,0 +1,4 @@
+const API_BASE_URL = 'http://localhost:3000';
+const IMG_PATH = 'https://github.com/TienNguyenDinh/training-javascript/blob/feature/add-product-functionality/javascript-practice/src/assets/images/product-images/product-image-name?raw=true';
+
+export { API_BASE_URL, IMG_PATH }
