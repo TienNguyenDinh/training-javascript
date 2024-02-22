87397b4f71e8cc8e75844fd90c3c7d9358d4acee
Tien Nguyen <149658780+TienNguyenDinh@users.noreply.github.com>
2024-02-22 01:21:17 +0000

    feat(js): add main js

--- javascript-practice/src/js/main.js
+++ javascript-practice/src/js/main.js
@@ -1,1 +1,9 @@
- 
+import ProductController from './controllers/product.controller';
+import ProductModel from './models/product.model';
+import ProductView from './views/product.view';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const productModel = new ProductModel();
+  const productView = new ProductView();
+  const productController = new ProductController(productModel, productView);
+});
