66681963618874effd5667b590a7c71e182abbcf
Tien Nguyen <mrfuree@gmail.com>
2024-02-22 16:54:45 +0700

    feat(js): add service to controller in main

--- javascript-practice/src/js/main.js
+++ javascript-practice/src/js/main.js
@@ -1,9 +1,11 @@
 import ProductController from './controllers/product.controller';
 import ProductModel from './models/product.model';
+import ProductService from './services/product.services';
 import ProductView from './views/product.view';
 
 document.addEventListener('DOMContentLoaded', () => {
   const productModel = new ProductModel();
   const productView = new ProductView();
-  const productController = new ProductController(productModel, productView);
+  const productService = new ProductService();
+  const productController = new ProductController(productModel, productView, productService);
 });
