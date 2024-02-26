3a453003e078a1adc4a738b6afe8d3bb4b8e4fd2
Tien Nguyen <mrfuree@gmail.com>
2024-02-26 11:38:18 +0700

    refactor: remove unused util method

--- javascript-practice/src/js/utils/createController.js
+++
@@ -1,7 +0,0 @@
-export default function createController({Controller, Model, View, Service}) {
-  const model = new Model();
-  const view = new View();
-  const service = new Service();
-
-  return new Controller(model, view, service);
-}
