b5b205715bdeb142d02f970c9d745c5d31cca931
Tien Nguyen <mrfuree@gmail.com>
2024-02-26 11:38:48 +0700

    feat(js): update routes for routing

--- javascript-practice/src/js/constants/config.js
+++ javascript-practice/src/js/constants/config.js
@@ -1,4 +1,11 @@
 const API_BASE_URL = 'http://localhost:3000';
 const IMG_PATH = 'https://github.com/TienNguyenDinh/training-javascript/blob/feature/add-product-functionality/javascript-practice/src/assets/images/product-images/product-image-name?raw=true';
+const ROUTES = {
+  HOME: '/',
+  PRODUCT_DETAIL: '/products/:id',
+  ADD_PRODUCT: '/add-product',
+  EDIT_PRODUCT: '/edit-product/:id',
+  CART: '/cart'
+}
 
-export { API_BASE_URL, IMG_PATH }
+export { API_BASE_URL, IMG_PATH, ROUTES }
