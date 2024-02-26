bd6ccd7f2f0c70d609f5f37ec1bbcaa4faa4ab19
Skeletonchampion <greensproblem00@gmail.com>
2024-02-27 04:00:17 +0700

    feat(router): add comments for router.js

--- javascript-practice/src/js/controllers/router.js
+++ javascript-practice/src/js/controllers/router.js
@@ -4,6 +4,11 @@ import { ROUTES } from '../constants/config';
 
 import matchRoute from '../utils/matchRoute';
 
+
+/**
+ * An object that maps route names to their respective handlers.
+ * @type {Object.<string, {handler: Function}>}
+ */
 const routes = {
   [ROUTES.HOME]: {
     handler: async () => {
@@ -38,19 +43,27 @@ const routes = {
 document.addEventListener('click', (e) => {
   const target = e.target;
 
-  if(!target.matches('a')) return;
+  if (!target.matches('a')) return;
 
   e.preventDefault();
 
   handleRoute(target);
 });
 
+/**
+ * Handles route changes. Updates the browser history and triggers a location change.
+ * @param {EventTarget} target - The link element.
+ */
 function handleRoute(target) {
   window.history.pushState(null, '', target.href);
 
   handleChangeLocation();
 }
 
+/**
+ * Handles location changes. Matches the current path to a route and calls the route's handler.
+ * @export
+ */
 export default function handleChangeLocation() {
   const pathName = window.location.pathname;
 
