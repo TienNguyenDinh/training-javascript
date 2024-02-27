1b07457f85160c32b3f181ed8d967f73abf8ec4d
Tien Nguyen <mrfuree@gmail.com>
2024-02-27 14:42:25 +0700

    fix(main): make productModel null as it is not used

--- javascript-practice/src/js/main.js
+++ javascript-practice/src/js/main.js
@@ -4,7 +4,7 @@ import ProductService from './services/product.services';
 import ProductView from './views/product.view';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const productModel = new ProductModel();
+  const productModel = null;
   const productView = new ProductView();
   const productService = new ProductService();
   const productController = new ProductController(productModel, productView, productService);
