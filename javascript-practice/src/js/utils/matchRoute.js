6e9a7373230b64b6ee94cee2be4ad6728a68b3be
Skeletonchampion <greensproblem00@gmail.com>
2024-02-28 03:34:10 +0700

    feat(route): add comments for matchRoute()

--- javascript-practice/src/js/utils/matchRoute.js
+++ javascript-practice/src/js/utils/matchRoute.js
@@ -1,21 +1,31 @@
 import { ROUTES } from '../constants/config';
 
+/**
+ * Matches a path name to a route in the ROUTES object
+ * If the route includes an ':id' parameter, it extracts the id from the path name
+ *
+ * @param {string} pathName - The path name to match
+ * @returns {{route: string, params: Object}} An object containing the matched route and any parameters
+ */
 export default function matchRoute(pathName) {
   for (let route in ROUTES) {
     const myRoute = ROUTES[route];
 
+    // If the route exactly matches the path name
     if (myRoute === pathName) {
-        return { route: myRoute, params: { } };
+      return { route: myRoute, params: {} };
     }
 
+    // If the route includes an ':id' parameter
     if (myRoute.includes(':id')) {
-        const regex = new RegExp(myRoute.replace(':id', '\\d+'));
+      // Create a regular expression to match the route, replacing ':id' with a digit pattern
+      const regex = new RegExp(myRoute.replace(':id', '\\d+'));
 
-        if (regex.test(pathName)) {
-          return { route: myRoute, params: { id: parseInt(pathName.split('/')[2]) } };
-        }
+      if (regex.test(pathName)) {
+        return { route: myRoute, params: { id: parseInt(pathName.split('/')[2]) } };
+      }
     }
   }
 
-  return { };
+  return {};
 }
