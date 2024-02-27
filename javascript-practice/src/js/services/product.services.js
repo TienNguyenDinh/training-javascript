5be45993db8f0e72d0c14459f7ce964941860548
Tien Nguyen <149658780+TienNguyenDinh@users.noreply.github.com>
2024-02-27 08:58:30 +0000

    fix(service): fix comment and rename variables

--- javascript-practice/src/js/services/product.services.js
+++ javascript-practice/src/js/services/product.services.js
@@ -1,18 +1,16 @@
-import { API_ENDPOINTS } from '../constants/config';
-
+import { API_ROUTES } from '../constants/config';
 import { API } from '../utils/api';
 
 export default class ProductService {
   /**
    * Gets products from the server.
-   * @async
    * @returns {Promise<Object[]>} An array of product objects
    */
   async getProducts() {
     try {
-      const fetchProductsURL = API_ENDPOINTS.GET_PRODUCTS_URL;
+      const fetchProductsEndpoint = API_ROUTES.GET_PRODUCTS_URL;
 
-      const data = await API.handleGet(fetchProductsURL);
+      const data = await API.handleGet(fetchProductsEndpoint);
 
       return data;
     } catch (error) {
