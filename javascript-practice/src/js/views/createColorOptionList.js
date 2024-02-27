fde9a96a83bc1e2b4209d5ba35b792b71c684b05
Tien Nguyen <mrfuree@gmail.com>
2024-02-27 16:46:18 +0700

    feat(view): use createNewElement instead document.createElement

--- javascript-practice/src/js/views/createColorOptionList.js
+++ javascript-practice/src/js/views/createColorOptionList.js
@@ -1,3 +1,5 @@
+import { createNewElement } from './dom';
+
 /**
  * Generates a list of color options for a product
  * @param {Object[]} colors An array of color object.
@@ -7,25 +9,29 @@ export default function createColorOptionList(colors) {
   const colorOptionList = [];
 
   for(let color of colors) {
-    const productColorWrapperElement = document.createElement('li');
+    const productColorWrapperElement = createNewElement('li');
 
     // Creating the label element for each color
-    const productColorLabelElement = document.createElement('label');
-    productColorLabelElement.className = 'btn btn-option-color';
-    productColorLabelElement.setAttribute('data-color', color.name);
-    productColorLabelElement.setAttribute('for', `color-${color.name}`);
+    const productColorLabelAttributes = {
+      'data-color': color.name,
+      for: color.name
+    }
+    const productColorLabelElement = createNewElement('label', 'btn btn-option-color', '', productColorLabelAttributes);
 
     // Creating the radio button element for each color
-    const productColorInputElement = document.createElement('input');
-    productColorInputElement.hidden = true;
-    productColorInputElement.setAttribute('type', 'radio')
-    productColorInputElement.setAttribute('id', `color-${color.name}`);
+    productColorInputAttributes = {
+      hidden: true,
+      type: 'radio',
+      id: `color-${color.name}`
+    }
+    const productColorInputElement = createNewElement('input', '', '', productColorInputAttributes);
+
     // Creating the object colorValue for product color input
-    let colorValueJson = JSON.stringify({
+    let colorValue = JSON.stringify({
       name: color.name,
       hexCode: color.hexCode
     });
-    productColorInputElement.setAttribute('value', colorValueJson);
+    productColorInputElement.setAttribute('value', colorValue);
     productColorInputElement.setAttribute('name', 'color');
 
     productColorWrapperElement.append(productColorLabelElement, productColorInputElement);
