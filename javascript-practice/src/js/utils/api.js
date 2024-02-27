0a3c51799e4d58c5bac6059d0ca64375631b43b4
Tien Nguyen <mrfuree@gmail.com>
2024-02-27 14:45:38 +0700

    fix(api): add method to API and return data instead of object containing data

--- javascript-practice/src/js/utils/api.js
+++ javascript-practice/src/js/utils/api.js
@@ -1,23 +1,23 @@
 import showToastify from '../utils/toastify';
 
-async function handleFetching(url) {
-  try {
-    const res = await fetch(url);
+const API = {
+  async handleGet(url) {
+    try {
+      const res = await fetch(url);
 
-    if(!res.ok) {
-      throw new Error(`Failed to fetch data from ${url}`);
+      if(!res.ok) {
+        throw new Error(`Failed to fetch data from ${url}`);
+      }
+
+      const data = await res.json();
+
+      return data;
+    } catch(error) {
+      console.error(error);
+
+      showToastify(error.message, 'toastify-danger');
     }
-
-    const data = await res.json();
-
-    return {
-      data
-    }
-  } catch(error) {
-    console.error(error);
-
-    showToastify(error.message, 'toastify-danger');
   }
 }
 
-export { handleFetching }
+export { API }
