908d450e3c2696813796e82e8086a008ee5629cb
Tien Nguyen <mrfuree@gmail.com>
2024-02-28 10:59:36 +0700

    fix(main): fix issue

--- javascript-practice/src/js/main.js
+++ javascript-practice/src/js/main.js
@@ -1,29 +1,17 @@
-<<<<<<< HEAD
-=======
-import ProductController from './controllers/product.controller';
-import ProductModel from './models/product.model';
-import ProductService from './services/product.service';
->>>>>>> feature/add-display-products
 import ProductView from './views/product.view';
-import ProductService from './services/product.services';
+import ProductService from './services/product.service';
 import ProductController from './controllers/product.controller';
 import handleChangeLocation from './controllers/router';
 
-let productController,
-  homeController,
-  productPageController;
+let productController;
 
 document.addEventListener('DOMContentLoaded', () => {
   const productView = new ProductView();
   const productService = new ProductService();
-<<<<<<< HEAD
 
-  productController = new ProductController(productModel, productView, productService);
+  productController = new ProductController(null, productView, productService);
 
   handleChangeLocation();
-=======
-  const productController = new ProductController(null, productView, productService);
->>>>>>> feature/add-display-products
 });
 
 export {
