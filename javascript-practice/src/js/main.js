6f2038bf8f3bcf55f0a3c27a33a840430acd8fff
Tien Nguyen <149658780+TienNguyenDinh@users.noreply.github.com>
2024-02-28 02:58:21 +0000

    fix(main): remove model since it is not used

--- javascript-practice/src/js/main.js
+++ javascript-practice/src/js/main.js
@@ -1,11 +1,10 @@
 import ProductController from './controllers/product.controller';
 import ProductModel from './models/product.model';
-import ProductService from './services/product.services';
+import ProductService from './services/product.service';
 import ProductView from './views/product.view';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const productModel = null;
   const productView = new ProductView();
   const productService = new ProductService();
-  const productController = new ProductController(productModel, productView, productService);
+  const productController = new ProductController(null, productView, productService);
 });
