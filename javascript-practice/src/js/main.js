96ef3d0a5180e744c3a2f48355ecb9b92b4716bc
Tien Nguyen <mrfuree@gmail.com>
2024-02-28 14:13:44 +0700

    fix(main): remove unnecessary imports and exports

--- javascript-practice/src/js/main.js
+++ javascript-practice/src/js/main.js
@@ -1,21 +1,5 @@
-import ProductView from './views/product.view';
-import ProductService from './services/product.service';
-import ProductController from './controllers/product.controller';
-import handleChangeLocation from './controllers/router';
-
-let productController;
+import handleChangeLocation from './routes/router';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const productView = new ProductView();
-  const productService = new ProductService();
-
-  productController = new ProductController(null, productView, productService);
-
   handleChangeLocation();
 });
-
-export {
-  productController,
-  homeController,
-  productPageController
-}
