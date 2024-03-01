4e11bbf95bc5a7c1caebe6552d82648671f9f546
Tien Nguyen <mrfuree@gmail.com>
2024-03-01 15:47:08 +0700

    feat(service): init 3 methods in productService

--- javascript-practice/src/js/services/product.service.js
+++ javascript-practice/src/js/services/product.service.js
@@ -1,3 +1,7 @@
+// TODO: Implement the add method to add a new product to the API
+// TODO: Implement the editById method to update a specific product in the API using its ID
+// TODO: Implement the removeById method to delete a specific product from the API using its ID
+
 import { API_ROUTES } from '../constants/url-api';
 import { APIHandler } from '../utils/api';
 
@@ -15,7 +19,7 @@ export default class ProductService {
   }
 
   /**
-   * Getches a product by its ID
+   * Gets a product by its ID
    * @param {string} id - The ID of the product
    * @returns {Promise<Object>} The object contains the product
    */
@@ -26,5 +30,28 @@ export default class ProductService {
     const data = await APIHandler.get(endpoint);
 
     return data;
+  }
+
+  /**
+   * Add a new product
+   */
+  async add() {
+
+  }
+
+  /**
+   * Edit a product by its ID
+   * @param {string} id - The ID of the product
+   */
+  async editById(id) {
+
+  }
+
+  /**
+   * Remove a product by its ID
+   * @param {string} id - The ID of the product
+   */
+  async removeById(id) {
+
   }
 }
