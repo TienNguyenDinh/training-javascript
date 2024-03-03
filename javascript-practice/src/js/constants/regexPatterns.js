491d5304aa418fc9b6e7246c380aa8e0d56a5fec
Skeletonchampion <greensproblem00@gmail.com>
2024-03-03 10:39:47 +0700

    feat(regex): add wordRegexStr

--- javascript-practice/src/js/constants/regexPatterns.js
+++ javascript-practice/src/js/constants/regexPatterns.js
@@ -1,4 +1,5 @@
 const REGEX_PATTERNS = {
+  wordRegexStr: '\\w+',
   digitRegex: /^-?\d*\.?\d+$/,
   hexCodeRegex: /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3}|[A-Fa-f0-9]{8})$/,
   camelCaseRegex: /([a-z])([A-Z])/g,
