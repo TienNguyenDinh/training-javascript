a7eb2365b79eb4d9a53d172b7c90450790e4dc8b
Skeletonchampion <greensproblem00@gmail.com>
2024-03-03 14:52:29 +0700

    feat(service): add getAll()

--- javascript-practice/src/js/services/cart.service.js
+++ javascript-practice/src/js/services/cart.service.js
@@ -1,7 +1,9 @@
-// TODO: Implement the getAll method to fetch all cart items from the API
 // TODO: Implement the add method to add a new cart item to the API
 // TODO: Implement the editById method to update a specific cart item in the API using its ID
 // TODO: Implement the removeById method to delete a specific cart item from the API using its ID
+
+import { API_ROUTES } from '../constants/url-api';
+import { APIHandler } from '../utils/api';
 
 export default class CartService {
   /**
@@ -9,7 +11,11 @@ export default class CartService {
    * @returns {Promise<Object[]>} An array of cart item objects
    */
   async getAll() {
+    const endpoint = API_ROUTES.CART_ENDPOINT;
 
+    const data = await APIHandler.get(endpoint);
+
+    return data;
   }
 
   /**
