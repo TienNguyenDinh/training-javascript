a7859a32118072393d703f58e6e826d183c6f746
Skeletonchampion <greensproblem00@gmail.com>
2024-03-03 10:43:18 +0700

    feat(view): add data-field into <input> and data-field-error to <p>

--- javascript-practice/src/js/views/product.view.js
+++ javascript-practice/src/js/views/product.view.js
@@ -120,53 +120,53 @@ export default class ProductView {
         <form id="product-form" action="javascript:void(0)" class="form-default add-form">
           <div class="flex-column">
             <label class="label-primary" for="name">Name</label>
-            <input id="name" type="text" class="form-control input-size-md">
-            <p class="error-message" id="name-error"></p>
+            <input data-field="Name" id="name" type="text" class="form-control input-size-md">
+            <p data-field-error="Name" class="error-message" id="name-error"></p>
           </div>
           <div class="flex-column">
             <label class="label-primary" for="price">Price</label>
-            <input id="price" type="text" class="form-control input-size-md">
-            <p class="error-message" id="price-error"></p>
+            <input data-field="Price" id="price" type="text" class="form-control input-size-md">
+            <p data-field-error="Price" class="error-message" id="price-error"></p>
           </div>
           <div class="flex-column">
             <label class="label-primary" for="brand">Brand</label>
-            <input id="brand" type="text" class="form-control input-size-md">
-            <p class="error-message" id="brand-error"></p>
+            <input data-field="Brand" id="brand" type="text" class="form-control input-size-md">
+            <p data-field-error="Brand" class="error-message" id="brand-error"></p>
           </div>
           <div class="flex-column">
             <label class="label-primary" for="model-name">Model Name</label>
-            <input id="model-name" type="text" class="form-control input-size-md">
-            <p class="error-message" id="modelName-error"></p>
+            <input data-field="Model Name" id="model-name" type="text" class="form-control input-size-md">
+            <p data-field-error="Model Name" class="error-message" id="modelName-error"></p>
           </div>
           <div class="flex-column">
             <label class="label-primary" for="color">Color</label>
-            <input id="color" type="text" class="form-control input-size-md">
-            <p class="error-message" id="color-error"></p>
+            <input data-field="Color" id="color" type="text" class="form-control input-size-md">
+            <p data-field-error="Color" class="error-message" id="color-error"></p>
           </div>
           <div class="flex-column">
             <label class="label-primary" for="hex-code">Hex Code</label>
-            <input id="hex-code" type="text" class="form-control input-size-md">
-            <p class="error-message" id="hexCode-error"></p>
+            <input data-field="Hex Code" id="hex-code" type="text" class="form-control input-size-md">
+            <p data-field-error="Hex Code" class="error-message" id="hexCode-error"></p>
           </div>
           <div class="flex-column">
             <label class="label-primary" for="form-factor">Form Factor</label>
-            <input id="form-factor" type="text" class="form-control input-size-md">
-            <p class="error-message" id="formFactor-error"></p>
+            <input data-field="Form Factor" id="form-factor" type="text" class="form-control input-size-md">
+            <p data-field-error="Form Factor" class="error-message" id="formFactor-error"></p>
           </div>
           <div class="flex-column">
             <label class="label-primary" for="connectivity-technology">Connectivity Technology</label>
-            <input id="connectivity-technology" type="text" class="form-control input-size-md">
-            <p class="error-message" id="connectivityTechnology-error"></p>
+            <input data-field="Connectivity Technology" id="connectivity-technology" type="text" class="form-control input-size-md">
+            <p data-field-error="Connectivity Technology" class="error-message" id="connectivityTechnology-error"></p>
           </div>
           <div class="flex-column">
             <label class="label-primary" for="amount">Amount</label>
-            <input id="amount" type="text" class="form-control input-size-md">
-            <p class="error-message" id="amount-error"></p>
+            <input data-field="Amount" id="amount" type="text" class="form-control input-size-md">
+            <p data-field-error="Amount" class="error-message" id="amount-error"></p>
           </div>
           <div class="flex-column">
             <label class="label-primary" for="image-url">Image URL</label>
-            <input id="image-url" type="text" class="form-control input-size-md">
-            <p class="error-message" id="imgUrl-error"></p>
+            <input data-field="Image URL" id="image-url" type="text" class="form-control input-size-md">
+            <p data-field-error="Image URL" class="error-message" id="imgUrl-error"></p>
           </div>
           <div class="flex-row">
             <button id="add-product" type="submit" class="btn btn-primary btn-submit">Submit</button>
