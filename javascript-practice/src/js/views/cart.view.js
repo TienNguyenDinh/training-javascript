e8d45a9b0e356d7001d8cea1ab4612b84a6aaf60
Tien Nguyen <mrfuree@gmail.com>
2024-03-04 09:51:17 +0700

    feat(view): remove demo cart data

--- javascript-practice/src/js/views/cart.view.js
+++ javascript-practice/src/js/views/cart.view.js
@@ -6,36 +6,18 @@ export default class CartView {
   /**
    * Renders list of cart items on the page
    */
-  renderCart() {
+  renderCart(cart) {
     const mainContent = getElementById('main-content');
-
-    // Test data
-    const cart = [
-      {
-        name: 'Adidas',
-        color: 'Black',
-        amount: 2,
-        price: 29.99,
-        imgUrl: 'https://ik.imagekit.io/kwzzowwcg/samsung-earphones-white.png'
-      },
-      {
-        name: 'Toshiba',
-        color: 'White',
-        amount: 1,
-        price: 49.99,
-        imgUrl: 'https://ik.imagekit.io/kwzzowwcg/samsung-earphones-white.png'
-      }
-    ];
 
     let cartListHTML = '<ul class="shopping-cart">'
     for (const item of cart) {
-      const { name, color, amount, price, imgUrl } = item;
+      const { id, name, color, amount, price, imgUrl } = item;
       const totalPrice = amount * price;
 
       const cartItemHTML = `
        <li class="cart-item">
         <div class="cart-item-image">
-          <button type="button" class="btn btn-primary">X</button>
+          <button id="btn-delete-${id} data-cart-item-id="${id}" type="button" class="btn btn-primary btn-delete-cart-item">X</button>
           <figure>
             <img src="${imgUrl}" alt="${name}" />
           </figure>
