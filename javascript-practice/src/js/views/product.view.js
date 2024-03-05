b190eba7892961a8e3ce3bf567f7bde16ea99761
Tien Nguyen <mrfuree@gmail.com>
2024-03-05 09:33:29 +0700

    feat(view): add edit btn to each product

--- javascript-practice/src/js/views/product.view.js
+++ javascript-practice/src/js/views/product.view.js
@@ -44,7 +44,16 @@ export default class ProductView {
               <img
               src="${imgUrl}" alt="${name}">
             </a>
-            <button id="btn-delete-${id}" data-id="${id}" class="btn btn-delete btn-primary"></button>
+            <div class="btn-group-action">
+              <button id="btn-delete-${id}" data-id="${id}" class="btn btn-delete btn-primary"></button>
+              <a href="/edit-product/${id}" class="btn btn-edit btn-primary">
+                <svg height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
+                  <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
+                </svg>
+              </a>
+            </div>
           </figure>
           <div class="product-details">
             <a href="${productHref}">
