98f5a35ffee571a763da5088643744a908669641
Tien Nguyen <mrfuree@gmail.com>
2024-03-05 10:22:29 +0700

    feat(api): make base_url take from env or local domain if it does not exist

--- javascript-practice/src/js/constants/url-api.js
+++ javascript-practice/src/js/constants/url-api.js
@@ -1,5 +1,5 @@
 const API_ROUTES = {
-  BASE_URL: 'https://js-practice-one-json-server.vercel.app',
+  BASE_URL: process.env.API_BASE_URL || 'http://localhost:3000',
   PRODUCTS_ENDPOINT: 'products',
   CART_ENDPOINT: 'cart'
 }
