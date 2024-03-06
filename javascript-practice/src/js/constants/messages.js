63265ee2676cd25ddb3a2a318ddf78f924b532e3
Tien Nguyen <mrfuree@gmail.com>
2024-03-06 17:19:14 +0700

    feat(constant): add constant messages

---
+++ javascript-practice/src/js/constants/messages.js
@@ -0,0 +1,11 @@
+const MESSAGES = {
+  NO_PRODUCT_LEFT_MSG: 'The product is running out of stock',
+  UPDATE_ITEM_FAILED_MSG: 'The item is not updated!',
+  INCREMENT_CART_FAILED_MSG: 'You cannot add more items!',
+  INCREMENT_CART_SUCCESS_MSG: 'You\'ve bought one more of this item!',
+  DELETE_CART_SUCCESS_MSG: 'The item is deleted!',
+  DELETE_CART_FAILED_MSG: 'This item can\'t be deleted right now!',
+  DELETE_CONFIRMATION_MSG: 'Are you sure that you want to delete this item?'
+}
+
+export default MESSAGES;
